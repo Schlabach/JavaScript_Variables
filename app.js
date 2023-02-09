b67a653b83a2255cf4b1970491517c3cbce9348e
Schlabach <matthewshawn89@gmail.com>
2023-02-09 06:44:47 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Matt";
+let lastName = "Schlabach";
+var age = 33;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+console.log(fullName);
 
 // Exercise 3
+let city = "Dalton";
+let pastime = "biking"
+let myStory = `\Hello! My name is ${fullName}. I live in ${city}. I enjoy ${pastime} and coding!`;
+console.log(myStory);
